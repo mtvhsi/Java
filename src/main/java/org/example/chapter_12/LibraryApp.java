5f51dec763970a15a1c83e53693e33d1301634cb
vhssh <littleraccon333@gmail.com>
2024-12-23 19:05:49 +0800

    10 исправлено

--- src/main/java/org/example/chapter_12/LibraryApp.java
+++ src/main/java/org/example/chapter_12/LibraryApp.java
@@ -8,47 +8,89 @@ import java.util.List;
 
 class Book {
     private final String title;
+    private final boolean canTakeHome;
+    private final Semaphore semBook;
+    private boolean otslez;
 
-    public Book(String title) {
+    public Book(String title, boolean canTakeHome){
         this.title = title;
+        this.canTakeHome = canTakeHome;
+        this.semBook = new Semaphore(1, true);
+        this.otslez = false;
     }
 
     public String getTitle() {
         return title;
+    }
+    public boolean canTakeHome() {
+        return canTakeHome;
+    }
+
+    public Semaphore getSemBook() {
+        return semBook;
+    }
+
+    public boolean setOtslez(boolean otsl){
+        this.otslez = otsl;
+        return otsl;
+    }
+
+    public boolean otslez(){
+        return otslez;
     }
 }
 
 class Library {
     private final List<Book> books;
     private final Semaphore semRead;
-    private final Semaphore semTake;
 
-    public Library(List<Book> books, int n, int m) {
+    public Library(List<Book> books, int n) {
         this.books = books;
         this.semRead = new Semaphore(n, true);
-        this.semTake = new Semaphore(m, true);
     }
 
     public void readInReadingRoom(Book b) throws InterruptedException {
+        if (b.otslez()) {
+            System.out.println(Thread.currentThread().getName() + " не может читать книгу " + b.getTitle() + " в читальном зале, она занята.");
+            return;
+        }
+
         semRead.acquire();
         try {
-            System.out.println(Thread.currentThread().getName() + " читает книгу в читальном зале: " + b.getTitle());
-            TimeUnit.SECONDS.sleep(1);
+            b.getSemBook().acquire();
+            try {
+                System.out.println(Thread.currentThread().getName() + " читает книгу в читальном зале: " + b.getTitle());
+                TimeUnit.SECONDS.sleep(1);
+            } finally {
+                b.getSemBook().release();
+            }
         } finally {
             semRead.release();
         }
     }
 
     public void takeHome(Book b) throws InterruptedException {
-        semTake.acquire();
-        try {
-            System.out.println(Thread.currentThread().getName() + " взял книгу на руки: " + b.getTitle());
-            TimeUnit.SECONDS.sleep(2);
-        } finally {
-            semTake.release();
+        if (b.canTakeHome()) {
+            if (!b.otslez()) {
+                b.getSemBook().acquire();
+                try {
+                    b.setOtslez(true);
+                    System.out.println(Thread.currentThread().getName() + " взял книгу на руки: " + b.getTitle());
+                    TimeUnit.SECONDS.sleep(2);
+                } finally {
+                    b.getSemBook().release();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + " не может взять книгу " + b.getTitle() + " на руки, она уже занята.");
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + " не может взять книгу " + b.getTitle() + " на руки.");
         }
     }
 }
+
+
+
 
 class User implements Runnable {
     private final Library lib;
@@ -78,12 +120,12 @@ class User implements Runnable {
 public class LibraryApp {
     public static void main(String[] args) {
         List<Book> books = new ArrayList<>();
-        books.add(new Book("Книга 1"));
-        books.add(new Book("Книга 2"));
-        books.add(new Book("Книга 3"));
-        books.add(new Book("Книга 4"));
+        books.add(new Book("Книга 1", true));
+        books.add(new Book("Книга 2",true));
+        books.add(new Book("Книга 3",true));
+        books.add(new Book("Книга 4", true));
 
-        Library lib = new Library(books, 2, 2);
+        Library lib = new Library(books, 2);
         ExecutorService exec = Executors.newFixedThreadPool(4);
 
         for (int i = 0; i < 4; i++) {
